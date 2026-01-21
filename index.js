4efe4711f4cd975ca746bc17b6d9be5ae85fb538
raposinha <elena@raposinha.dev>
2026-01-21 17:13:40 +0100

    finishes basic part of th exercise

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 const booksURL = "https://striveschool-api.herokuapp.com/books";
-
+const bookCardsContainerEl = document.getElementById("cards-container");
 const getData = function () {
   fetch(booksURL)
     .then((res) => res.json())
 
     .then((arrayOfBooks) => {
-      const bookCardsContainerEl = document.getElementById("cards-container");
       for (let book of arrayOfBooks) {
-        console.log(book);
         bookCardsContainerEl.innerHTML += `
-        <div class="col-4 mb-4">
+        <div class="col-3 mb-4">
           <div class="card h-100">
           <img class="card-img-top" src="${book.img}" alt="book cover" />
             <div class="card-body">
               <h5 class="card-title">${book.title}</h5>
               <p class="card-text">${book.price}</p>
-              <a href="#" class="btn btn-danger" id="discard-btn">Discard</a>
+              <a href="#" class="btn btn-danger discard-btn">Discard</a>
             </div>
              </div>
         </div>`;
@@ -24,5 +22,13 @@ const getData = function () {
     .catch((err) => {
       console.log("error", err);
     });
+
+  bookCardsContainerEl.addEventListener("click", function (e) {
+    //only deletes if the the click is on the button
+    if (e.target.classList.contains("discard-btn")) {
+      e.preventDefault();
+      e.target.parentElement.parentElement.parentElement.remove();
+    }
+  });
 };
 getData();
