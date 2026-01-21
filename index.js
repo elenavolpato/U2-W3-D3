0322843498c31c28c2db712b904bd7055a9ecaac
raposinha <elena@raposinha.dev>
2026-01-21 18:12:45 +0100

    finishes exercise

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const booksURL = "https://striveschool-api.herokuapp.com/books";
 const bookCardsContainerEl = document.getElementById("cards-container");
+
 const getData = function () {
   fetch(booksURL)
     .then((res) => res.json())
@@ -7,7 +8,7 @@ const getData = function () {
     .then((arrayOfBooks) => {
       for (let book of arrayOfBooks) {
         bookCardsContainerEl.innerHTML += `
-        <div class="col-4 mb-4">
+        <div class="col-4 mb-4" id="${book.asin}">
           <div class="card h-100">
           <img class="card-img-top" src="${book.img}" alt="book cover" />
             <div class="card-body d-flex flex-column justify-content-between">
@@ -21,28 +22,61 @@ const getData = function () {
              </div>
         </div>`;
       }
+
+      bookCardsContainerEl.addEventListener("click", function (e) {
+        //only deletes if the the click is on the button
+        if (e.target.classList.contains("discard-btn")) {
+          e.preventDefault();
+          e.target.parentElement.parentElement.parentElement.parentElement.remove();
+        }
+        // Add to cart button
+        if (e.target.classList.contains("add-to-cart")) {
+          e.preventDefault();
+          const cardDiv = e.target.parentElement.parentElement.parentElement;
+          const title = cardDiv.querySelector(".card-title").textContent;
+          const price = parseFloat(cardDiv.querySelector(".card-text").textContent.replace("€", ""));
+          const id = cardDiv.parentElement.id;
+          console.log(cardDiv.parentElement.id);
+
+          addToCart(title, price, id);
+        }
+      });
     })
     .catch((err) => {
       console.log("error", err);
     });
-
-  bookCardsContainerEl.addEventListener("click", function (e) {
-    //only deletes if the the click is on the button
-    if (e.target.classList.contains("discard-btn")) {
-      e.preventDefault();
-      e.target.parentElement.parentElement.parentElement.remove();
-    }
-    if (e.target.classList.contains("add-to-cart")) {
-      e.preventDefault();
-      console.log(e.target);
-    }
-  });
 };
 getData();
 
-const addToCart = () => {
-  if (e.target.classList.contains("add-to-cart")) {
-    e.preventDefault();
-    console.log(e.target);
+const cart = [];
+
+const addToCart = (bookName, bookPrice, bookId) => {
+  let addedBook = {
+    title: bookName,
+    price: bookPrice,
+    id: bookId,
+  };
+  cart.push(addedBook);
+
+  const cardList = document.getElementById("card-list");
+  const bookEl = document.createElement("div");
+
+  //bookEl.className = "d-flex justify-content-between border-bottom";
+
+  //adds a line with the total price of the cart
+  let sumPrice = cart.reduce((acc, book) => acc + parseFloat(book.price), 0);
+  let totalPriceEl = document.getElementById("total-price");
+
+  if (!totalPriceEl) {
+    totalPriceEl = document.createElement("div");
+    totalPriceEl.id = "total-price";
+    totalPriceEl.classList = "fw-bold border-bottom px-2 pt-2";
+    cardList.appendChild(totalPriceEl);
   }
+  totalPriceEl.innerHTML = `<p>Total: € ${sumPrice}</p>`;
+  cardList.innerHTML += `
+    <div class="d-flex justify-content-between border-bottom px-2 pt-2"> 
+        <p>${bookName}</p> 
+        <p>€ ${bookPrice}</p> 
+    </div>`;
 };
